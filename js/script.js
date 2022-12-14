a97ff12d2ae307ec95625b262bbc1ab532dfe84f
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2022-12-14 19:19:47 +0400

    JS-6
    JS-6

--- js/script.js
+++ js/script.js
@@ -1,120 +1,61 @@
-// // // //// //111111111// //// //// //// //
+"use strict";
 
-// let user = {
-//     firstname: "giorgi",
-//     lastname: "smith",
-//     age: 25,
-//     studentstatus: "active"
-//   };
 
-// console.log(user.studentstatus);
+
+
+let navigElements = document.getElementById('idnavbar');
+let burgerElements = document.getElementById('idburger');
+
+burgerElements.addEventListener('click', function(){
+    navigElements.classList.toggle('visible');
+})
+
+
+
+
 
 ////////////////////////////////////////////
 
-//  ყველა Loop კოდი მუშაობს //
 
-// let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
-
-//FOR LOOPS//
-
-// for(let i = 0; i < numbers.length; i++){
-//     console.log(numbers[i])
-
-// for (let element of numbers){
-//     console.log(element);
-// }
-
-// /////////WHILE LOOP /////////
-
-// let i = 0;
-// while(i < numbers.length){
-//     console.log(numbers[i]);
-//     i++;
-// }
-
-////////////////////333333//////////////////////
-
-// let ciprebi = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
-// for (let i = 1; i < ciprebi.length; i++){
-// if (ciprebi[i] > 5);
-// console.log(ciprebi[i]);
-// }
-
-////////////////////44444444444//////////////////////
-
-// let user = {
-//   name: 'giorgi',
-//   age:  20,
-//   studentStatus: 'active'
-// }
-
-// if(user.age < 18 && user.studentStatus == 'active'){
-// console.log('Hello');
-// }
-// if (user.name == 'levani'){
-// console.log('Hello levan');
-// }
-// if(user.studentStatus == 'active' || user.age < 25){
-// console.log('Hello World');
-// } else {
-//   console.log('error');
-// }
+let ulItem = document.querySelector('.ul-item');
+let addItem = document.querySelector('.item-add');
+let inputForm = document.querySelector('.input-form');
+let allClear = document.querySelector('.clear-all');
+let formElement = document.querySelector('.form-wrapper')
 
 
 
-////////////////////555555555//////////////////////
+formElement.addEventListener('submit', function(event){
+    event.preventDefault();
+let inputValue = inputForm.value;
+if(inputValue == ' '){
+    return;
+}
 
-// let array = [ 'watermelon', 'pear', 10, 45, 50, 'apple', 'ananas'];
+let li = document.createElement('li');
+let deleteBtn = document.createElement('i');
+deleteBtn.classList.add('fa-sharp', 'fa-solid', 'fa-trash');
+deleteBtn.style.cursor = 'pointer';
+deleteBtn.style.marginLeft = '10px';
+li.style.listStyle = 'none';
+li.style.padding = '4pxa'
+deleteBtn.addEventListener('click',function(){
+    li.remove();
+})
 
-// console.log(array[0], array[1], array[6], array[7]);
+////////////////////////////
 
-
-
-
-////////////////////666666666//////////////////////
-
-// let array = [ [2, -3, 5, 10], [25, -24, -11, 100] , [-6, -7, 10]];
-
-// for (let item of array) {
-//   for (let x of item) {
-//       if (x > 0) {
-//           console.log(x);
-//       }
-//   }
-// }
-
-
-
-////////////////////777777777//////////////////////
-
-
-// let array = [ 2, 3, 5, 10,25,24,11, 100 , 6,7, 10 ]
-// for (let item of array) {
-//   if(item%2 == 0){
-//     console.log(item)
-//   }
-// }
-
-// let array = [ 2, 3, 5, 10,25,24,11, 100 , 6,7, 10 ]
-// for (let kent of array) {
-//   if(kent%2 == 1){
-//     console.log(kent)
-//   }
-// }
+li.textContent = inputValue;
+ulItem.appendChild(li);
+li.appendChild(deleteBtn);
 
 
-////////////////////88888888//////////////////////
+inputForm.value = ' ';
+
+});
+
+allClear.addEventListener('click', function(){
+    ulItem.innerHTML = ' ';
+})
 
 
-
-// let users = [
-//   { username: 'giorgi', status: false },
-//   { username: 'levani', status: false },
-//   { username: 'anna', status: true },
-// ];
-
-// for (let item of users) {
-//       if (item.status == true) {
-//           console.log(item.username);
-//       }
-//   }
